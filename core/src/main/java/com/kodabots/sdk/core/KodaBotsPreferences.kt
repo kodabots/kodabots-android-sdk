c1b500cc0e0c0ed70e1ef525484e42ffa0d04e02
Bartosz Wyspiański <bartek@fream.pl>
2021-03-08 09:37:56 +0100

    MASTER #comment quick workaround of bug in EncryptedSharedPreferences library

--- core/src/main/java/com/kodabots/sdk/core/KodaBotsPreferences.kt
+++ core/src/main/java/com/kodabots/sdk/core/KodaBotsPreferences.kt
@@ -2,13 +2,14 @@ package com.kodabots.sdk.core
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.util.Log
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
-import androidx.security.crypto.MasterKeys
 
 object KodaBotsPreferences {
     private lateinit var preferences: SharedPreferences
     private val PREFERENCES_NAME = "KodaBots.pref"
+    private val UNSECURED_PREFERENCES_NAME = "KodaBotsUnsecured.pref"
     private val PREF_VERSION = "version"
 
     private var preferencesVersion: Int
@@ -16,17 +17,32 @@ object KodaBotsPreferences {
         set(version) = preferences.edit().putInt(PREF_VERSION, version).apply()
 
     fun initialize(context: Context) {
-        val masterKey = MasterKey.Builder(context)
-            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
-            .build()
+        initializeEncrypted(context)
+    }
 
-        preferences = EncryptedSharedPreferences.create(
-            context,
-            PREFERENCES_NAME,
-            masterKey,
-            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-        )
+    @Synchronized
+    private fun initializeEncrypted(context: Context){
+        try {
+            val masterKey = MasterKey.Builder(context)
+                .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+                .build()
+
+            preferences = EncryptedSharedPreferences.create(
+                context,
+                PREFERENCES_NAME,
+                masterKey,
+                EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            )
+        } catch (e:Exception){
+            //WORKAROUND FOR https://issuetracker.google.com/issues/176215143
+            Log.e("KodaBotsSDK","Failed to create encrypted shared preferences ${e::class.java.name} ${e.message}")
+            initializeUnsecured(context)
+        }
+    }
+
+    private fun initializeUnsecured(context: Context){
+        preferences = context.getSharedPreferences(UNSECURED_PREFERENCES_NAME, Context.MODE_PRIVATE)
     }
 
     private val KEY_USER_ID = "KEY_USER_ID"
